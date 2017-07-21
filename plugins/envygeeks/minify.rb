f580ac5a8df45d131173279b600691053e2b115c
Jordon Bedwell <jordon@envygeeks.io>
2017-07-20 22:48:39 -0500

    Lets rework it so it's fast.  It seems the other way is slow on Docker.

--- plugins/envygeeks/minify.rb
+++ plugins/envygeeks/minify.rb
@@ -2,44 +2,21 @@
 # Copyright 2016 - 2017 Jordon Bedwell - MIT License
 # Encoding: UTF-8
 
-require "open3"
-
-module EnvyGeeks
+if Jekyll.env == "development"
 
   # --
-  # Allows you to minify the sites HTML the same way it
-  # will be in production so that you get a valid look at
-  # how the site will behave, because minification can
-  # remove a lot of whitespae.
+  # We need this to remain 99 because if it doesn't then
+  # there will be problems with things like livereload, which should
+  # really prioritize themselves as being the very last.
   # --
-  class Minify
-    def initialize(input)
-      @input = input
-    end
 
-    def run
-      status, out, err = nil, "", ""
-      Open3.popen3("script/minify") do |i, o, e, p|
-        i.puts @input; i.close
-        out = o.read
-        err = e.read
+  Jekyll::Hooks.register :site, :post_write, :priority => 99 do |site|
+    out = `script/minify`
 
-        if !p.value.success?
-          out = @input
-          if err
-            Jekyll.logger.error(
-              err
-            )
-          end
-        end
-      end
-
-      out
+    if $?.exitstatus != 0
+      Jekyll.logger.error(
+        "Unable to Minify"
+      )
     end
   end
-end
-
-# Register and run it.
-Jekyll::Hooks.register [:pages, :documents, :posts], :post_render do |doc|
-  doc.output = EnvyGeeks::Minify.new(doc.output).run
 end
