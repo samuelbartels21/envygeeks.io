0b4b2b9b762f1cdd6ea921c0b63d801d1d1ca1ec
Jordon Bedwell <jordon@envygeeks.io>
2019-04-11 23:04:31 -0500

    Add the server files.

---
+++ gridsome.server.js
@@ -0,0 +1,23 @@
+/**
+ * Learn more:
+ * https://gridsome.org/docs/server-api
+ */
+module.exports = function(api) {
+  api.loadSource(store => {
+  	store.addMetaData("siteSubTitle", "Envygeeks")
+  	store.addMetaData("siteTitle", "Jordon Bedwell")
+  	store.addMetaData("siteUrl", "envygeeks.io")
+  	store.addMetaData("siteNav", [
+  		{
+  			id: 1,
+  			title: "Home",
+  			to: "/"
+  		},
+  		{
+  			id: 2,
+  			title: "About",
+  			to: "/about"
+  		}
+  	])
+  })
+}
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import DefaultLayout from '~/layouts/default.vue'
+export default function (Vue, { router, head, isClient }) {
+  Vue.component('Layout', DefaultLayout)
+
+  /**
+   * Google Fonts
+   * @todo Eventually this should be hosted
+   *   by me to remove privacy concerns.
+   */
+  head.link.push({
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css?family=" +
+    	"Lora:300,300i,400,400i|" +
+      "Montserrat:300,300i,400,400i,700,700i|" +
+      "Noto+Sans:400,400i"
+  })
+}
