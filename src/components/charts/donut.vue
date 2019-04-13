b1f4aae17a771b93ab09bdf1616cf6f1124e9eda
Jordon Bedwell <jordon@envygeeks.io>
2019-04-13 01:51:36 -0500

    Add a donut chart.

---
+++ src/components/charts/donut.vue
@@ -0,0 +1,27 @@
+<script>
+  import { Doughnut as Donut } from "vue-chartjs";
+  import { shuffled } from "~/assets/colors";
+
+  export default {
+    extends: Donut,
+    name: "donut",
+    props: [
+      "labels",
+      "values"
+    ],
+
+    mounted () {
+      this.renderChart({
+        labels: this.labels,
+        datasets: [{
+          backgroundColor: shuffled(),
+          data: this.values
+        }]
+      }, {
+        legend: {
+          display: false
+        }
+      })
+    }
+  }
+</script>
