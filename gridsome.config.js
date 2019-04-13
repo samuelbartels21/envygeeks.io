c370736b058a2be516d5e9eccf2b142a52a10d29
Jordon Bedwell <jordon@envygeeks.io>
2019-04-13 11:10:51 -0500

    Configure Posts, and Pages.

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,4 +1,43 @@
 module.exports = {
   siteName: "envygeeks.io",
-  plugins: []
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'src/posts/**/*.md',
+        route: '/blog/:year/:month/:day/:slug',
+        typeName: 'post',
+        refs: {
+          tags: {
+            typeName: 'tag',
+            route: '/tag/:id',
+            create: true
+          }
+        }
+      }
+    },
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        route: "/:slug",
+        path: "src/pages/**/*.md",
+        typeName: 'page',
+      }
+    }
+  ],
+
+  transformers: {
+    remark: {
+      externalLinksTarget: "_blank",
+      anchorClassName: "icon icon-link",
+      externalLinksRel: [
+        "nofollow",
+        "noreferrer",
+        "noopener"
+      ],
+      plugins: [
+        "@gridsome/remark-prismjs"
+      ]
+    }
+  }
 }
