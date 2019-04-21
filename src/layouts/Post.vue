2d1d8830d630916828a5cfb96c597a4c3692c047
Jordon Bedwell <jordon@envygeeks.io>
2019-04-21 13:23:05 -0500

    Add a post layout.

---
+++ src/layouts/Post.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout>
+    <main role=main :class="many ? 'content posts' : 'content'">
+      <slot/>
+    </main>
+  </Layout>
+</template>
+
+<script>
+  import Layout from "~/layouts/Default.vue"
+  export default {
+    name: "Post",
+    components: {
+      Layout
+    },
+
+    props: [
+      "many"
+    ]
+  }
+</script>
