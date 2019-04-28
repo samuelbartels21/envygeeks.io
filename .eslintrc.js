fc8320fa7e68135015f2e1d152a3a350932038c9
Jordon Bedwell <jordon@envygeeks.io>
2019-04-27 22:45:04 -0500

    🙃 Refactor eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,16 @@ module.exports = {
   extends: "standard",
   env: { browser: true, es6: true },
   globals: { SharedArrayBuffer: "readonly", Atomics: "readonly" },
-  parserOptions: { sourceType: "module", ecmaVersion: 2018 }
-  plugins: ["vue"]
-  rules: {}
+  parserOptions: { sourceType: "module", ecmaVersion: 2018 },
+  plugins: ["vue"],
+  rules: {
+    "space-before-function-paren": [2, {
+        "anonymous": "always",
+        "asyncArrow": "always",
+        "named": "never",
+    }],
+    "quotes": [2,
+      "double"
+    ]
+  }
 }
